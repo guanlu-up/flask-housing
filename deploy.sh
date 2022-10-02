28aacaab0c3ba55d6676f477ef7f912e54f0d6f1
guanlu-up <guanlu.up@gmail.com>
2022-10-02 17:50:46 +0800

    add redis service

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 # $(pwd) 表示在项目路径下
+
 # 运行flask镜像服务
 docker run -it --rm \
   -v $(pwd):/root/src/app \
@@ -17,3 +18,14 @@ docker run -d \
   -v $(pwd)/cachedata/mysql/data:/var/lib/mysql \
   -v $(pwd)/cachedata/mysql/conf:/etc/mysql/conf.d \
   mysql
+
+# 运行redis服务
+docker run -d \
+  --name redis-homeapp \
+  --log-opt max-size=100m \
+  --log-opt max-file=2 \
+  -p 6379:6379 \
+  -v $(pwd)/cachedata/redis/myredis.conf:/etc/redis/redis.conf \
+  -v $(pwd)/cachedata/redis/data:/data \
+  redis
+
