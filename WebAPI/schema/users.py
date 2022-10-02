b97f847322333241159679afbd4bec39f7afd01f
guanlu-up <guanlu.up@gmail.com>
2022-10-02 18:00:29 +0800

    update attribute

--- WebAPI/schema/users.py
+++ WebAPI/schema/users.py
@@ -6,7 +6,12 @@ class UserSchema(Schema):
     id = fields.Integer()
     username = fields.String()
     is_admin = fields.Boolean()
+    is_delete = fields.Boolean()
+    phone = fields.String()
+    real_name = fields.String()
+    id_card = fields.String()
+    avatar_url = fields.String()
 
-    create_time = fields.DateTime()
+    start_time = fields.DateTime()
     update_time = fields.DateTime()
 
