3089017f2c12e4c7e1e800510a4192d00760fd30
guanlu-up <guanlu.up@gmail.com>
2022-10-03 16:12:50 +0800

    add redis

--- WebAPI/config.py
+++ WebAPI/config.py
@@ -1,10 +1,10 @@
 import datetime
 
-# import redis
+import redis
 
 
 class DATABASE(object):
-    HOST = "100.100.20.163"
+    HOST = "192.168.0.105"
     PORT = "3306"
     USERNAME = "root"
     PASSWORD = "root123"
@@ -21,13 +21,13 @@ class EnvironConfig(object):
     JWT_ACCESS_TOKEN_EXPIRES = datetime.timedelta(hours=1)
     JWT_REFRESH_TOKEN_EXPIRES = datetime.timedelta(days=1)
 
-    # REDIS_HOST = "127.0.0.1"
-    # REDIS_PORT = 6379
-    #
-    # SESSION_TYPE = "redis"
-    # SESSION_REDIS = redis.StrictRedis(host=REDIS_HOST, port=REDIS_PORT)
-    # SESSION_USE_SIGNER = True   # 对cookie中的session_id进行混淆处理
-    # PERMANENT_SESSION_LIFETIME = (60 * 60) * 24     # session数据的有效期,单位秒
+    REDIS_HOST = "192.168.0.105"
+    REDIS_PORT = 6379
+
+    SESSION_TYPE = "redis"
+    SESSION_REDIS = redis.StrictRedis(host=REDIS_HOST, port=REDIS_PORT)
+    SESSION_USE_SIGNER = True   # 对cookie中的session_id进行混淆处理
+    PERMANENT_SESSION_LIFETIME = (60 * 60) * 24     # session数据的有效期,单位秒
 
 
 class DevelopmentConfig(EnvironConfig):
