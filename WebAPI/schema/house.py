dc9c5fd797d01cd5e704aeb222050f7d2df83438
guanlu-up <guanlu.up@gmail.com>
2022-10-06 22:34:53 +0800

    add house schema

---
+++ WebAPI/schema/house.py
@@ -0,0 +1,33 @@
+from marshmallow import Schema, fields
+
+
+class CityAreaSchema(Schema):
+    id = fields.Integer()
+    name = fields.String()
+
+
+class HouseSchema(Schema):
+    id = fields.Integer()
+    title = fields.String()
+    price = fields.Integer()
+    address = fields.String()
+    room_count = fields.Integer()
+    acreage = fields.Integer()
+    unit = fields.String()
+    capacity = fields.Integer()
+    beds = fields.String()
+    deposit = fields.Integer()
+    min_days = fields.Integer()
+    max_days = fields.Integer()
+    order_count = fields.Integer()
+    image_url = fields.String()
+
+
+class FacilitySchema(Schema):
+    id = fields.Integer()
+    name = fields.String()
+
+
+class HouseImageSchema(Schema):
+    id = fields.Integer()
+    url = fields.String()
