d61e82444a9d91e19c57003425fb68ec0475f03c
guanlu-up <guanlu.up@gmail.com>
2022-10-06 22:35:31 +0800

    update view: house_view, versatile_view

--- WebAPI/views/__init__.py
+++ WebAPI/views/__init__.py
@@ -3,9 +3,13 @@ from flask import Flask
 from .welcome import welcome_view
 from .users import users_view
 from .auth import auth_view
+from .house import house_view
+from .versatile import versatile_view
 
 
 def init_views(app: Flask):
     app.register_blueprint(welcome_view)
     app.register_blueprint(users_view)
     app.register_blueprint(auth_view)
+    app.register_blueprint(house_view)
+    app.register_blueprint(versatile_view)
