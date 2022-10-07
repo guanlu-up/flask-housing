e027d2d86874e8c388516da50a888262e23a1f32
guanlu-up <guanlu.up@gmail.com>
2022-10-07 18:15:31 +0800

    add Versatile type

--- WebAPI/constants.py
+++ WebAPI/constants.py
@@ -5,3 +5,8 @@ class Auth(object):
     # 当账户错误次数达到阈值时被锁定的时间,单位:秒
     ACCOUNT_LOCKOUT_TIME = (60 * 5)
 
+
+class Versatile(object):
+    # 城区名称信息在redis中保存的时长,单位:秒
+    CITY_AREAS_TIME = (60 * 60) * 2
+
