b062c5714daca6d3da17ab223cc9e46cfec51bc1
guanlu-up <guanlu.up@gmail.com>
2022-10-10 10:47:55 +0800

    modify /api output

--- WebAPI/views/welcome.py
+++ WebAPI/views/welcome.py
@@ -5,7 +5,8 @@ welcome_view = Blueprint("welcome", __name__, url_prefix="/api")
 
 @welcome_view.route("", methods=["GET"])
 def helloworld():
-    return {"satus": 200, "message": "helloworld 中国"}
+
+    return {"satus": 200, "message": "hello 欢迎访问HomeApp的API"}
 
 
 @welcome_view.route("/welcome", methods=["GET"])
