17678252f607581f93189d5063db5940f085eda7
guanlu-up <guanlu.up@gmail.com>
2022-10-10 16:58:33 +0800

    add Houses constant

--- WebAPI/constants.py
+++ WebAPI/constants.py
@@ -10,3 +10,12 @@ class Versatile(object):
     # 城区名称信息在redis中保存的时长,单位:秒
     CITY_AREAS_TIME = (60 * 60) * 2
 
+
+class Houses(object):
+
+    # 返回当前销量最好的房源时limit限制
+    HOT_HOUSES_LIMIT = 5
+    # 当前销量最好的房源信息在redis中保存的key name
+    HOT_HOUSES_SIGN = "CurrentHotHouses"
+    # 当前销量最好的房源信息在redis中保存的时间
+    HOT_HOUSES_TIME = (60 * 60)
